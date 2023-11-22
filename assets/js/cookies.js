e4b65fe6326bc63207c974d680ba3f2d81f65feb
lorensansol <lorensansol@gmail.com>
2023-11-22 14:41:34 +0100

    Update submodule: 2023-11-22 14:41:34

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -5,14 +5,30 @@ const cookiesMessageAcept = document.querySelectorAll('.cookies__button-acept')
 const cookiesMessageToogle = document.querySelector('.cookies__toggle')
 
 if (cookiesMessage && cookiesMessageAcept && cookiesMessageToogle) {
-  // Add class active from cookies message if cookies don't acept
-  localStorage.controlcookie && cookiesMessage.classList.add('cookies--hide')
+
+  function cookiesOpen() {
+    cookiesMessage.removeAttribute('hidden')
+    cookiesMessage.querySelector('[data-h]').focus()
+    setTimeout(() => cookiesMessage.classList.remove('cookies--hide'), 10)
+  }
+  function cookiesClose() {
+    cookiesMessage.classList.add('cookies--hide')
+    setTimeout(() => cookiesMessage.setAttribute('hidden', 'until-found'), 300)
+  }
+  function cookiesToggle() {
+    const isCookiesOpen = document.querySelector('.cookies[hidden="until-found"]')
+    if (isCookiesOpen) {
+      cookiesOpen()
+    } else {
+      cookiesClose()
+    }
+  }
 
   // Onclick cookies accept
   function cookiesAccept(c) {
     localStorage.controlcookie = localStorage.controlcookie || 0
     localStorage.controlcookie++
-    cookiesMessage.classList.add('cookies--hide')
+    cookiesClose()
     // If acept all or if not acept all and if analytics ckecked
     const aceptAll = c.target.classList.value.includes('cookies__button-acept--all')
     const analyticsCkecked = document.querySelector('[value="de-analisis"]').checked
@@ -20,17 +36,19 @@ if (cookiesMessage && cookiesMessageAcept && cookiesMessageToogle) {
       localStorage.controlcookieanalytics = localStorage.controlcookieanalytics || 0
       localStorage.controlcookieanalytics++
       {{/* if site.Data.config.cookies_legal */}}
-      {{ with partial "functions/lang-param" (dict "parent" "config" "param" "google_analytics") }}
-        googleAnalytics()
+      {{ if hugo.IsProduction }}
+        {{ with partial "functions/lang-param" (dict "parent" "config" "param" "google_analytics") }}
+          googleAnalytics()
+        {{ end }}
       {{ end }}
     }
   }
   cookiesMessageAcept.forEach(e => e.addEventListener('click', c => cookiesAccept(c)))
 
+  // Add class active from cookies message if cookies don't acept
+  if (localStorage.controlcookie) cookiesClose()
+
   // Onclick cookies message toggle
-  function cookiesToggle() {
-    cookiesMessage.classList.toggle('cookies--hide')
-  }
-  cookiesMessageToogle.addEventListener('click', () => cookiesToggle())
-  cookiesMessageToogle.addEventListener('keydown', c => c.key === 'Enter' && cookiesToggle())
+  cookiesMessageToogle.addEventListener('click', cookiesToggle)
+  cookiesMessageToogle.addEventListener('keydown', e => e.key === 'Enter' && cookiesMessageToogle.click())
 }
--- assets/js/mails.js
+++ assets/js/mails.js
@@ -4,14 +4,19 @@ document.querySelectorAll('.ofuscate').forEach(mail => {
 })
 
 document.querySelectorAll('.contact__email-option-copy, .contact__email-option-send').forEach(emailCopyButtons => {
+  // emailCopyButtons.addEventListener('keydown', e => e.key === 'Enter' && emailCopyButtons.click())
   emailCopyButtons.addEventListener('click', emailCopyButton => {
+    const button = emailCopyButton.currentTarget
     const email = emailCopyButton.target
       .closest('.contact__email')
       .querySelector('.ofuscate')
       .textContent
     // If copy button then copy in clipbard; if not then send email
-    if (emailCopyButton.currentTarget.classList[0].indexOf('copy') > -1) {
+    if (button.classList[0].indexOf('copy') > -1) {
       navigator.clipboard.writeText(email)
+      const msg = 'contact__email-option-copy--active'
+      button.classList.add(msg)
+      setTimeout(() => button.classList.remove(msg), 1000)
     } else {
       window.location.href = 'mailto:' + email
     }
