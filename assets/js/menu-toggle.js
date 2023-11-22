f978873b1051fea4b4b85fe1dee6c88adc4cc447
lorensansol <lorensansol@gmail.com>
2023-11-22 21:07:42 +0100

    Update submodule: 2023-11-22 21:07:42

--- assets/js/menu-toggle.js
+++ assets/js/menu-toggle.js
@@ -3,18 +3,19 @@
 
 const menu = document.querySelector('.menu')
 if (menu) {
-  const menuSticky = document.querySelector('.menu--sticky')
   const menuToggleButton = document.querySelector('.menu__toggle')
   const menuBackover = document.querySelector('.menu__backover')
   const menuLinks = document.querySelectorAll('.menu__link:not(.menu__item--more > .menu__link, .menu__link--translate), .menu__button')
+  const menuSticky = document.querySelector('.menu--sticky')
   const menuBreackPoint = {{ site.Data.menu.screen_sticky | default 0 }}
+  const menuNoStickyVisibility = () => !menuSticky || window.innerWidth < menuBreackPoint
   function menuOpen() {
     menu.removeAttribute('hidden')
     menu.focus()
     document.documentElement.classList.add('menu__active')
   }
   function menuClose() {
-    if (!menuSticky || window.innerWidth < menuBreackPoint) {
+    if (menuNoStickyVisibility()) {
       document.documentElement.classList.remove('menu__active')
       setTimeout(() => menu.setAttribute('hidden', 'until-found'), 300)
     }
@@ -28,7 +29,7 @@ if (menu) {
     }
   }
   function menuVisibility() {
-    if (window.innerWidth < menuBreackPoint) {
+    if (menuNoStickyVisibility()) {
       menu.setAttribute('hidden', 'until-found')
     } else {
       menu.removeAttribute('hidden')
