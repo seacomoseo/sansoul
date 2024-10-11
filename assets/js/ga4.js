20f28ad27481ccd1adedee3a7be0b9bfda321a38
lorensansol <lorensansol@gmail.com>
2024-10-11 17:37:56 +0200

    Update submodule: 2024-10-11 17:37:56

--- assets/js/ga4.js
+++ assets/js/ga4.js
@@ -24,26 +24,26 @@ export function initGa4 () {
 
   function ga4 (e) {
     const button = e.currentTarget
-    let ofuscateLink, category, label, value
+    let ofuscateLink, category, label = ''
 
     if (button.href) {
-      value = button.href
+      label = button.href
     } else if (button.dataset.b) {
       ofuscateLink = button.dataset.b
     } else if (button.dataset.h) {
       ofuscateLink = button.dataset.h
     } else {
-      value = button.parentElement.previousElementSibling.textContent
+      label = button.parentElement.previousElementSibling.textContent
     }
 
     if (ofuscateLink) {
       const link = window.atob(ofuscateLink)
       if (button.classList.contains('callnow__phone') || button.classList.contains('contact__phone')) {
-        value = link.replace('tel:+', '')
+        label = link.replace('tel:+', '')
       } else if (button.classList.contains('callnow__whatsapp') || button.classList.contains('contact__whatsapp')) {
-        value = link.replace('https://wa.me/', '')
+        label = link.replace('https://wa.me/', '')
       } else {
-        value = link
+        label = link
       }
     }
 
@@ -54,26 +54,26 @@ export function initGa4 () {
     }
 
     if (button.classList.toString().search('phone') !== -1) {
-      label = 'phone'
+      label = `phone: ${label}`
     } else if (button.classList.toString().search('whatsapp') !== -1) {
-      label = 'whatsapp'
+      label = `whatsapp: ${label}`
     } else if (button.classList.contains('contact__email-option-copy')) {
-      label = 'email_copy'
+      label = `email_copy: ${label}`
     } else if (button.classList.contains('contact__email-option-send')) {
-      label = 'email_send'
+      label = `email_send: ${label}`
     } else if (button.classList.contains('contact__address')) {
-      label = 'address'
+      label = `address: ${label}`
     } else if (button.classList.contains('contact__map')) {
-      label = 'map'
+      label = `map: ${label}`
     } else if (button.classList.toString().search('custom--ga4') !== -1) {
-      label = 'custom'
+      label = `custom: ${label}`
     }
 
     // eslint-disable-next-line
     gtag('event', 'contact_click', {
       event_category: category,
       event_label: label,
-      value
+      value: 1
     })
   }
 
