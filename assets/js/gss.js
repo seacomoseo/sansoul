e0227e6117dc0149bbeff77ef34ac63b30a0b4fc
lorensansol <lorensansol@gmail.com>
2025-07-21 19:52:03 +0200

    Fix GSS

--- assets/js/gss.js
+++ assets/js/gss.js
@@ -1,16 +1,21 @@
 // google spreadsheets
 import { scrollShot } from './scroll-shot'
-import { loadScript } from './load-script'
 
 export function initGss () {
   scrollShot({
     rootMargin: '600px 0%',
     query: '[data-gss]',
     doStart: e => {
-      loadScript(e.dataset.gss)
+      fetch(e.dataset.gss)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+          }
+          return response.text()
+        })
         .then(data => {
           // display response
-          e.innerHTML = data.responseText
+          e.innerHTML = data
             .replace(/\n/ig, '')
             .replace(/<!DOCTYPE .*<link .*?>|style> .*?<body .*?>|<script .*?<\/script>|<style>.*?<\/style>|html \{.*?\.dash \{.*?\}|(font-family|padding):.*?;|display:none;|<thead>.*?<\/thead>|<\/body><\/html>/ig, '')
           document.querySelectorAll('.row-header-shim').forEach(e => e.remove())
